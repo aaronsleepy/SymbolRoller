0ae1856aeaba6659988b9bef4b2d610d70c59757
aaronsleepy <aaron@kmong.com>
2022-11-19 11:03:33 +0900

    feat: 커스텀 View Controller 추가 및 View-Controller 연동
    - @IBOutlet: UIObject와 Controller outlet 연결
    - @IBAction: UIObject와 Controller action 연결

---
+++ SymbolRoller/SymbolRulerViewController.swift
@@ -0,0 +1,50 @@
+//
+//  SymbolRullerViewController.swift
+//  SymbolRoller
+//
+//  Created by Aaron on 2022/11/19.
+//
+
+import UIKit
+
+class SymbolRulerViewController: UIViewController {
+    
+    let symbols: [String] = ["sun.min", "moon", "cloud", "wind", "snowflake"]
+
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var button: UIButton!
+    
+    // 앱 메모리 로드 완료
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        imageView.image = UIImage(systemName: "cloud")
+        label.text = "로드 완료"
+    }
+    
+    // 이제 화면 뜰꺼야
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+    
+    // 화면 떳다
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+    }
+    
+    @IBAction func buttonTapped(_ sender: Any) {
+        label.text = "와 눌렸다"
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
